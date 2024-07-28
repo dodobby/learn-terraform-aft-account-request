ca0e7bf36f0f51f7bd0b7dc643cfa422b5c39bd2
dodobby <142660660+dodobby@users.noreply.github.com>
2024-07-28 17:39:36 +0900

    Add main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,61 @@
+# ACCOUNT REQUEST FOR cn-account1
+
+module "cn-account1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "alchdli.odd+cnaccount1@gmail.com"
+    AccountName               = "cn-account1"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "alchdli.odd+cnaccount1@gmail.com"
+    SSOUserFirstName          = "cn-account1"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "cn-account1"
+}
+
+
+# ACCOUNT REQUEST FOR cn-account2
+
+module "cn-account2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "alchdli.odd+cnaccount2@gmail.com"
+    AccountName               = "cn-account2"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "alchdli.odd+cnaccount2@gmail.com"
+    SSOUserFirstName          = "cn-account2"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "cn-account2"
+}
+
