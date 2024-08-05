c9e173ddd550fcb76cf6737547c10058828719b5
dodobby <142660660+dodobby@users.noreply.github.com>
2024-08-05 15:36:06 +0900

    Add main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
-# ACCOUNT REQUEST FOR cn-account5
+# ACCOUNT REQUEST FOR cn-account6
 
-module "cn-account5" {
+module "cn-account6" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "alchdli.odd+cnaccount5@gmail.com"
-    AccountName               = "cn-account5"
+    AccountEmail              = "alchdli.odd+cnaccount6@gmail.com"
+    AccountName               = "cn-account6"
     ManagedOrganizationalUnit = "AFT"
-    SSOUserEmail              = "alchdli.odd+cnaccount5@gmail.com"
-    SSOUserFirstName          = "cn-account5"
+    SSOUserEmail              = "alchdli.odd+cnaccount6@gmail.com"
+    SSOUserFirstName          = "cn-account6"
     SSOUserLastName           = "AFT"
   }
 
@@ -25,6 +25,6 @@ module "cn-account5" {
     group = "non-prod"
   }
 
-  account_customizations_name = "cn-account5"
+  account_customizations_name = "cn-account6"
 }
 
