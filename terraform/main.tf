6c135a3ee5d8beb134a8d4f0fe97e0aa48d071c1
xxobby <alchdli.odd@gmail.com>
2025-01-08 00:12:10 +0900

    mod vars to yaml

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,56 @@
+# module "sandbox" {
+#   source = "./modules/aft-account-request"
+
+#   control_tower_parameters = {
+#     AccountEmail              = "alchdli.odd+lgu@gmail.com"
+#     AccountName               = "qpie-aft-lgu"
+#     ManagedOrganizationalUnit = "Sandbox"
+#     SSOUserEmail              = "alchdli.odd+lgu@gmail.com"
+#     SSOUserFirstName          = "Sandbox"
+#     SSOUserLastName           = "AFT"
+#   }
+
+#   account_tags = {
+#     "Learn Tutorial" = "AFT"
+#   }
+
+#   change_management_parameters = {
+#     change_requested_by = "HashiCorp Learn"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#   }
+
+#   custom_fields = {
+#     group = "non-prod"
+#   }
+
+#   account_customizations_name = "sandbox"
+# }
+
+locals {
+  config = yamldecode(file("./sandbox_config.yaml"))
+}
+
 module "sandbox" {
-  source = "./modules/aft-account-request"
+  for_each = { for account in local.config.accounts : account.AccountName => account }
+  source   = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "alchdli.odd+lgu@gmail.com"
-    AccountName               = "qpie-aft-lgu"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "alchdli.odd+lgu@gmail.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = each.value.control_tower_parameters.AccountEmail
+    AccountName               = each.value.control_tower_parameters.AccountName
+    ManagedOrganizationalUnit = each.value.control_tower_parameters.ManagedOrganizationalUnit
+    SSOUserEmail              = each.value.control_tower_parameters.SSOUserEmail
+    SSOUserFirstName          = each.value.control_tower_parameters.SSOUserFirstName
+    SSOUserLastName           = each.value.control_tower_parameters.SSOUserLastName
   }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
+  account_tags = each.value.account_tags
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = each.value.change_management_parameters.change_requested_by
+    change_reason       = each.value.change_management_parameters.change_reason
   }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+  custom_fields = each.value.custom_fields
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = each.value.account_customizations_name
 }
